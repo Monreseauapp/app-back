828ea5d1c3456fb8f67fb1643d96a135d45ab107
zogeek <91289024+zogeek@users.noreply.github.com>
2025-06-13 15:26:45 +0200

    fix: standardize formatting and improve readability in various files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   if (!process.env.PORT) {
-    console.warn('No PORT environment variable set, defaulting to 3000');
+    console.warn('No PORT environment variable set, defaulting to 3000')
   }
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
