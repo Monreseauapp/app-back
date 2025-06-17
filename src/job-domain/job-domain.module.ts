b9ed2f61155cc27dbbd9e7a8062bb104f086a8a3
zogeek <91289024+zogeek@users.noreply.github.com>
2025-06-17 16:44:14 +0200

    feat: implement Company, JobDomain, Subscription, and Users modules with controllers and services

---
+++ src/job-domain/job-domain.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { JobDomainService } from './job-domain.service';
+import { JobDomainController } from './job-domain.controller';
+
+@Module({
+  controllers: [JobDomainController],
+  providers: [JobDomainService],
+})
+export class JobDomainModule {}
---
+++ src/subscription/subscription.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class SubscriptionService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+
+  create(createSubscriptionDto: Prisma.SubscriptionCreateInput) {
+    return this.databaseService.subscription.create({
+      data: createSubscriptionDto,
+    });
+  }
+
+  findAll() {
+    return this.databaseService.subscription.findMany();
+  }
+
+  findOne(id: string) {
+    return this.databaseService.subscription.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateSubscriptionDto: Prisma.SubscriptionUpdateInput) {
+    return this.databaseService.subscription.update({
+      where: { id },
+      data: updateSubscriptionDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.databaseService.subscription.delete({
+      where: { id },
+    });
+  }
+}
