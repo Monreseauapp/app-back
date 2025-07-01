ec6878f7dce84178854e979863e61546f6fa3e7a
zogeek <91289024+zogeek@users.noreply.github.com>
2025-07-01 14:25:27 +0200

    Enable CORS for all routes in the bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Enable CORS for all routes
+  app.enableCors({
+    origin: ['http://localhost:8001', 'http://localhost:3000'], // Add your frontend URLs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  
+  await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
